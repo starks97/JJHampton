998e2d434207d303df4662097f0d937df947ba1a
Jefferson <ifrit68@hotmail.com>
2024-03-26 19:17:28 -0400

    production2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,4 @@
 import { defineConfig } from "astro/config";
-<<<<<<< HEAD
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel()
-});
-=======
-
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
@@ -20,4 +6,3 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
 });
->>>>>>> 3092a264ad40f4de8a845a0235d4c1cd8b0a8b0d
