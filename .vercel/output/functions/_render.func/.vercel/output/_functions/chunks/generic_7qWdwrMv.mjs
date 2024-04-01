f53cbbc4098e0af8452e69195cfeb6e0e1b882f8
Jefferson <ifrit68@hotmail.com>
2024-04-01 19:22:43 -0400

    update noder version and sharp

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_7qWdwrMv.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_CxTAtxPa.mjs').then(n => n.g);
-
-export { page };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(),
+    react(),
+    image({
+      service: squooshImageService(),
+    }),
+  ],
 });
