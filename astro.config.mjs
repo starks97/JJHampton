5fbf25bbc664657602a0c88eda9966981cc7493b
Jefferson <ifrit68@hotmail.com>
2024-04-01 19:31:08 -0400

    vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig, squooshImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import image from "@astrojs/image";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-    image({
-      service: squooshImageService(),
-    }),
-  ],
+  integrations: [tailwind(), react(), image({
+    service: squooshImageService()
+  })],
+  output: "server",
+  adapter: vercel()
 });
