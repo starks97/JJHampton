cadbd83013df60e9f3bb47fe0aff72026fee432b
Jefferson <ifrit68@hotmail.com>
2024-04-01 21:02:44 -0400

    vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig, squooshImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
-
 import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
 export default defineConfig({
